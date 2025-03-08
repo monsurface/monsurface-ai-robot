f2e1a6b0970a1d279f19b5015fcb9cd36ce579b3
monsurface <service@monsurface.com>
2025-03-08 23:25:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def ask_chatgpt(user_question):
     {formatted_text}
 
     用戶的問題是：「{user_question}」
-    請根據建材資料以條列式回答問題。
+    請根據建材資料以條列式回答問題，且都使用繁體中文。
     如果問題與建材無關，請回答：「這個問題與建材無關，我無法解答。」。
     """
 
